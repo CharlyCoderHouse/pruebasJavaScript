26c0cdb45cdb329cd158d660f4b0ee8bbda4cbfe
CharlyCoderHouse <carlosdiblasi@gmail.com>
2022-07-13 20:07:00 -0300

    Terminado el desafio Primer Entrega

--- Clase7_Desafio/index.js
+++ Clase7_Desafio/index.js
@@ -3,31 +3,31 @@
 // Al final se mostrará el pedido con el total a pagar
 
 class Menu {
-    constructor(id, nombre, precio, numero) {
+    constructor(id, nombre, precio, numero, cantidad) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.numero = numero;
-        this.pedido = false;
-        this.cantidad = 0;
+        this.cantidad = cantidad;
+        this.pedido = false;       
     }
     seleMenu(){
-        this.pedido = true;
         this.cantidad = this.cantidad + 1;
+        this.pedido = true;       
     }
 }
 
 // Inicializo el menú de la carta 
 const cartaMenu = [];
-cartaMenu.push(new Menu(1, "Ñoquis a la Bolognesa", 1200, 1));
-cartaMenu.push(new Menu(1, "Matambre al Verdeo", 2500, 2));
-cartaMenu.push(new Menu(1, "Bife de Chorizo con papas españolas", 3500, 3));
-cartaMenu.push(new Menu(2, "Gaseosa", 500, 4));
-cartaMenu.push(new Menu(2, "Cerveza", 700, 5));
-cartaMenu.push(new Menu(2, "Vino", 900, 6));
-cartaMenu.push(new Menu(3, "Helado", 480, 7));
-cartaMenu.push(new Menu(3, "Flan con dulce", 350, 8));
-cartaMenu.push(new Menu(3, "Ensalada de Frutas", 500, 9));
+cartaMenu.push(new Menu(1, "Ñoquis a la Bolognesa", 1200, 1, 0));
+cartaMenu.push(new Menu(1, "Matambre al Verdeo", 2500, 2, 0));
+cartaMenu.push(new Menu(1, "Bife de Chorizo con papas españolas", 3500, 3, 0));
+cartaMenu.push(new Menu(2, "Gaseosa", 500, 4, 0));
+cartaMenu.push(new Menu(2, "Cerveza", 700, 5, 0));
+cartaMenu.push(new Menu(2, "Vino", 900, 6, 0));
+cartaMenu.push(new Menu(3, "Helado", 480, 7, 0));
+cartaMenu.push(new Menu(3, "Flan con dulce", 350, 8, 0));
+cartaMenu.push(new Menu(3, "Ensalada de Frutas", 500, 9, 0));
 
 // Inicializo las variables a usar
 const pedidoCliente = [];
@@ -88,19 +88,25 @@ while (entrada != "5") {
             case 2:
                 // Seleccion de la categoría 1 Plato Principal
                 const platoFiltro = cartaMenu.filter((elPlato) => elPlato.id == 1);
+               
                 for (const platoPrincipal of platoFiltro){
                     elPedido += platoPrincipal.numero + ".- " + platoPrincipal.nombre + " - Precio: $ " + platoPrincipal.precio + "\n";
                 }
+                // Le pregunto al cliente que seleccione el plato y valido que la elección sea correcta
                 let plato1 = prompt("Seleccione un plato de las siguientes opciones: \n" + elPedido);
                 if (platoFiltro.some((el) => el.numero == parseInt(plato1))) {
                     
                     const ordena = platoFiltro.find((el) => el.numero == plato1 );
-                    pedidoCliente.push(ordena);
-                    
+                    // Si es la primera vez lo cargo al pedido, sino le sumo solo la cantidad
+                    if (ordena.cantidad == 0){
+                        pedidoCliente.push(ordena);
+                    }
+
                     for (const venta of platoFiltro) {
                         if(venta.numero == plato1){
                             venta.seleMenu()
                         }
+                        
                     }
                 }else{
                     // Sino encontro una opción correcta vuelve al menú anterior
@@ -108,46 +114,66 @@ while (entrada != "5") {
                     elPedido = "";
                     break;
                 }
+                
                 elPedido = "";
                 break;
 
             case 3:
+                // Seleccion de la categoría 2 Bebidas
                 const bebidaFiltro = cartaMenu.filter((laBebida) => laBebida.id == 2);
+
                 for (const bebidaPrincipal of bebidaFiltro){
                     elPedido += bebidaPrincipal.numero + ".- " + bebidaPrincipal.nombre + " - Precio: $ " + bebidaPrincipal.precio + "\n";
                 }
+                // Le pregunto al cliente que seleccione el plato y valido que la elección sea correcta
                 let bebida1 = prompt("Seleccione una Bebida de las siguientes opciones: \n" + elPedido);
                 if (bebidaFiltro.some((el) => el.numero == parseInt(bebida1))) {
+
                     const ordena = bebidaFiltro.find((el) => el.numero == bebida1 );
-                    pedidoCliente.push(ordena);
+                    // Si es la primera vez lo cargo al pedido, sino le sumo solo la cantidad
+                    if (ordena.cantidad == 0){
+                        pedidoCliente.push(ordena);
+                    }
+
                     for (const venta of bebidaFiltro) {
                         if(venta.numero == bebida1){
                             venta.seleMenu()
                         }
                     }
                 }else{
+                    // Sino encontro una opción correcta vuelve al menú anterior
                     alert("Ingresaste una opción fuera del rango solicitado!\n Intente nuevamente ! Usted Ingreso: " + bebida1);
                     elPedido = "";
                     break;
                 }
+
                 elPedido = "";
                 break;                
 
             case 4:
+                // Seleccion de la categoría 3 Postre
                 const postreFiltro = cartaMenu.filter((elPostre) => elPostre.id == 3);
+
                 for (const postrePrincipal of postreFiltro){
                     elPedido += postrePrincipal.numero + ".- " + postrePrincipal.nombre + " - Precio: $ " + postrePrincipal.precio + "\n";
                 }
+                // Le pregunto al cliente que seleccione el plato y valido que la elección sea correcta
                 let postre1 = prompt("Seleccione una Bebida de las siguientes opciones: \n" + elPedido);
                 if (postreFiltro.some((el) => el.numero == parseInt(postre1))) {
+
                     const ordena = postreFiltro.find((el) => el.numero == postre1 );
-                    pedidoCliente.push(ordena);
+                    // Si es la primera vez lo cargo al pedido, sino le sumo solo la cantidad
+                    if (ordena.cantidad == 0){
+                        pedidoCliente.push(ordena);
+                    }
+
                     for (const venta of postreFiltro) {
                         if(venta.numero == postre1){
                             venta.seleMenu()
                         }
                     }
                 }else{
+                    // Sino encontro una opción correcta vuelve al menú anterior
                     alert("Ingresaste una opción fuera del rango solicitado!\n Intente nuevamente ! Usted Ingreso: " + postre1);
                     elPedido = "";
                     break;
@@ -159,15 +185,26 @@ while (entrada != "5") {
                 break;
         }
     } else {
+        // Sino encontro una opción correcta vuelve al menú anterior
         alert("Ingresaste una opción fuera del rango solicitado!. Ingreso: " + entrada);
     }
-
+    // Vuelvo a preguntar las opciones hasta que finalice
     entrada = prompt("Ingrese una de las siguientes opciones para realizar su pedido: \n1.- Visualizar la carta completa  \n2.- Elegir el Plato principal \n3.- Elegir la Bebida \n4.- Elegir el Postre \n5.- Confirmar el pedido");
 
 }
 
-const precioPedido = pedidoCliente.reduce( (acumulador, precio) => acumulador + precio.precio, 0)
-for(finalPedido of pedidoCliente){
-    elPedido += finalPedido.numero + ".- " + finalPedido.nombre + "\n";
+// Verifico que se haya seleccionado algo para calcular el pedido
+if (pedidoCliente.length > 0){
+    // Calculo el precio final
+    const precioPedido = pedidoCliente.reduce( (acumulador, precio) => acumulador + (precio.precio * precio.cantidad), 0)
+    for(finalPedido of pedidoCliente){
+        elPedido += finalPedido.numero + ".- " + finalPedido.nombre + " - Cant.: " +  finalPedido.cantidad + "\n";
+    }
+    // Informo el pedido realizado por el cliente
+    alert("Su pedido es: \n" + elPedido + "\n" + "Total a pagar: $ " + precioPedido + "\n Muchas Gracias!");
+
+}else{
+
+    alert("No selecciono nada de la carta, vuelva a intentar realizar su pedido. Muchas Gracias!");
+
 }
-alert("Su pedido es: \n" + elPedido + "\n" + "Total a pagar: $ " + precioPedido + "\n Muchas Gracias!");
